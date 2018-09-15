82f8cb3701cf21b57cfac57ecd6b4247a870a9f4
Shiyuan Guo <sguo35@gmail.com>
2018-09-15 10:56:55 -0700

    fix parser

--- Parse.py
+++ Parse.py
@@ -1,5 +1,7 @@
 lines = []
 
+import string
+
 def find_nth(haystack, needle, n):
     start = haystack.find(needle)
     while start >= 0 and n > 1:
@@ -7,15 +9,19 @@ def find_nth(haystack, needle, n):
         n -= 1
     return start
 
-with open('./movie_lines.txt', 'rb') as movie_lines:
-    movie_lines = movie_lines.read().decode(errors='ignore')
-    for line in movie_lines:
-        # get rid of bullshit starting lines
-        index = find_nth(line, "+++$+++", 4)
-        index = index + 7
-        new_line = line[index:]
+with open('./parsed_lines.txt', 'w') as parsed_lines:
+    with open('./movie_lines.txt', 'rb') as movie_lines:
+        movie_lines = movie_lines.read().decode(errors='ignore')
+        movie_lines = movie_lines.split('\n')
+        new_str = ""
+        for line in movie_lines:
+            # get rid of bullshit starting lines
+            index = find_nth(line, "+++$+++", 4)
+            index = index + 7
+            new_line = line[index:]
+            import re
+            new_line = re.sub(r'[^\w\s]','',new_line)
+            new_line = new_line.lower()
 
-        lines.append(new_line)
-
-import pickle
-pickle.dump(lines, open('./parsed_lines.pkl', 'wb'))
+            new_str += new_line
+        print(new_str, file=parsed_lines)            
