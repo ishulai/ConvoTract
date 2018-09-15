439580e67ab6656916d7633f6e44f1375b222c26
Thomas Lai <thomas990428@me.com>
2018-09-15 10:18:01 -0700

    Updated word2vec

--- Word2vec.py
+++ Word2vec.py
@@ -1,11 +1,76 @@
-import keras
+from keras.models import Model
+from keras.layers import Input, Dense, Reshape, merge
+from keras.layers.embeddings import Embedding
+from keras.preprocessing.sequence import skipgrams
+from keras.preprocessing import sequence
+
+import urllib
+import collections
+import os
+import zipfile
+
+import numpy as np
+import tensorflow as tf
+
+def maybe_download(filename, url, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        filename, _ = urllib.urlretrieve(url + filename, filename)
+    statinfo = os.stat(filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', filename)
+    else:
+        print(statinfo.st_size)
+        raise Exception(
+            'Failed to verify ' + filename + '. Can you get to it with a browser?')
+    return filename
+
+
+# Read the data into a list of strings.
+def read_data(filename):
+    """Extract the first file enclosed in a zip file as a list of words."""
+    with zipfile.ZipFile(filename) as f:
+        data = tf.compat.as_str(f.read(f.namelist()[0])).split()
+    return data
+
+
+def build_dataset(words, n_words):
+    """Process raw inputs into a dataset."""
+    count = [['UNK', -1]]
+    count.extend(collections.Counter(words).most_common(n_words - 1))
+    dictionary = dict()
+    for word, _ in count:
+        dictionary[word] = len(dictionary)
+    data = list()
+    unk_count = 0
+    for word in words:
+        if word in dictionary:
+            index = dictionary[word]
+        else:
+            index = 0  # dictionary['UNK']
+            unk_count += 1
+        data.append(index)
+    count[0][1] = unk_count
+    reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+    return data, count, dictionary, reversed_dictionary
+
+def collect_data(vocabulary_size=10000):
+    url = 'http://mattmahoney.net/dc/'
+    filename = maybe_download('text8.zip', url, 31344016)
+    vocabulary = read_data(filename)
+    print(vocabulary[:7])
+    data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
+                                                                vocabulary_size)
+    del vocabulary  # Hint to reduce memory.
+    return data, count, dictionary, reverse_dictionary
 
 vocab_size = 10000
 data, count, dictionary, reverse_dictionary = collect_data(vocabulary_size=vocab_size)
+print(data[:7])
 
 window_size = 3
 vector_dim = 300
-epochs = 1000000
+epochs = 200000
 
 valid_size = 16     # Random set of words to evaluate similarity on.
 valid_window = 100  # Only pick dev samples in the head of the distribution.
@@ -24,7 +89,6 @@ input_target = Input((1,))
 input_context = Input((1,))
 
 embedding = Embedding(vocab_size, vector_dim, input_length=1, name='embedding')
-
 target = embedding(input_target)
 target = Reshape((vector_dim, 1))(target)
 context = embedding(input_context)
@@ -38,13 +102,13 @@ dot_product = merge([target, context], mode='dot', dot_axes=1)
 dot_product = Reshape((1,))(dot_product)
 # add the sigmoid output layer
 output = Dense(1, activation='sigmoid')(dot_product)
-
 # create the primary training model
 model = Model(input=[input_target, input_context], output=output)
 model.compile(loss='binary_crossentropy', optimizer='rmsprop')
 
 # create a secondary validation model to run our similarity checks during training
 validation_model = Model(input=[input_target, input_context], output=similarity)
+
 
 class SimilarityCallback:
     def run_sim(self):
@@ -64,8 +128,8 @@ class SimilarityCallback:
         sim = np.zeros((vocab_size,))
         in_arr1 = np.zeros((1,))
         in_arr2 = np.zeros((1,))
+        in_arr1[0,] = valid_word_idx
         for i in range(vocab_size):
-            in_arr1[0,] = valid_word_idx
             in_arr2[0,] = i
             out = validation_model.predict_on_batch([in_arr1, in_arr2])
             sim[i] = out
@@ -81,7 +145,7 @@ for cnt in range(epochs):
     arr_2[0,] = word_context[idx]
     arr_3[0,] = labels[idx]
     loss = model.train_on_batch([arr_1, arr_2], arr_3)
-    if i % 100 == 0:
+    if cnt % 100 == 0:
         print("Iteration {}, loss={}".format(cnt, loss))
     if cnt % 10000 == 0:
         sim_cb.run_sim()
